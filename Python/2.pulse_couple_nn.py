cba1d02bc326d85fa87499e6ddd32cbd91041f75
X.Bookman <Bookman@vip.163.com>
2015-01-10 20:04:56 +0800

    Python-2

--- Python/2.pulse_couple_nn.py
+++ Python/2.pulse_couple_nn.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python
 
+# First execute: easy_install Image
+
+import math;
+import random;
+from PIL import Image;
+
+im = Image.open("/Users/Bookman/Pictures/29411.jpg");
+mat = im.getdata();
+im.show();
+
+class pulse_couple_nn:
+	
